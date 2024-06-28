b4bcac73bcb4bfa325b66d7709a96730ad876498
Terry <terryluan12@gmail.com>
2024-06-27 22:19:12 -0400

    Example to show Strict mode + createParentDirectories issues

--- src/util/fileSystem.ts
+++ src/util/fileSystem.ts
@@ -4,6 +4,7 @@ import { fs } from "@zenfs/core";
 import { Fetch, Overlay } from "@zenfs/core";
 import { IndexedDB } from "@zenfs/dom";
 import publicFileSystemIndex from "../../index.json";
+import { useEffect } from "react";
 
 const useTestFileSystem = () => {
   const config = {
@@ -21,20 +22,22 @@ const useTestFileSystem = () => {
       },
     },
   };
-  configure(config).then(() => {
-    fs.readFile("/desktop/dir/test1", { encoding: "utf-8" }, (err, res) => {
-      console.log("READ");
-      console.log("ERR: ", err);
-      console.log("RES: ", res);
+  useEffect(() => {
+    configure(config).then(() => {
+      // test();
+      fs.readFile("/desktop/dir/test", { encoding: "utf-8" }, (err, res) => {
+        console.log("ERR: ", err);
+        console.log("RES: ", res);
+      });
+      fs.readFile("/desktop/dir/test2", { encoding: "utf-8" }, (err, res) => {
+        console.log("ERR1: ", err);
+        console.log("RES1: ", res);
+      });
+
+      // fs.readdir("/desktop", (_, res) => {
+      //   console.log("READDIR", res);
+      // });
     });
-    // fs.readFile("/desktop/dir/test2", { encoding: "utf-8" }, (err, res) => {
-    //   console.log("READ");
-    //   console.log("ERR1: ", err);
-    //   console.log("RES1: ", res);
-    // });
-    // fs.readdir("/desktop", (_, res) => {
-    //   console.log("READDIR", res);
-    // });
   });
 };
 
